2b4484e5ea8df8d0431d3acb688d72650a0082a5
Hinageshi <2247001665@qq.com>
2020-09-15 23:55:12 +0800

    添加两种泥球王的材质 泥球王生成时血量具有一定随机性

--- src/main/java/chongteam/soulforging/client/renderer/RenderDirtBallKing.java
+++ src/main/java/chongteam/soulforging/client/renderer/RenderDirtBallKing.java
@@ -8,10 +8,16 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderDirtBallKing extends RenderLiving {
+public class RenderDirtBallKing extends RenderLiving{
     private static final ResourceLocation ENTITY_DIRT_BALL_KING_TEXTURE=
             new ResourceLocation(SoulForging.MODID + ":textures/entity/"
                     + EntityDirtBallKing.ID + "/" + EntityDirtBallKing.ID + ".png");
+    private static final ResourceLocation ENTITY_DIRT_BALL_KING_GRASS_TEXTURE=
+            new ResourceLocation(SoulForging.MODID + ":textures/entity/"
+                    + EntityDirtBallKing.ID + "/" + EntityDirtBallKing.ID + "_grass.png");
+    private static final ResourceLocation ENTITY_DIRT_BALL_KING_DIRT_TEXTURE=
+            new ResourceLocation(SoulForging.MODID + ":textures/entity/"
+                    + EntityDirtBallKing.ID + "/" + EntityDirtBallKing.ID + "_dirt.png");
 
     public RenderDirtBallKing(RenderManager manager){
         super(manager,new ModelDirtBallKing(),0.8F);
@@ -19,6 +25,13 @@ public class RenderDirtBallKing extends RenderLiving {
 
     @Override
     protected ResourceLocation getEntityTexture(Entity entity){
+        byte color=((EntityDirtBallKing) entity).getColor();
+        if(color == 1){
+            return ENTITY_DIRT_BALL_KING_DIRT_TEXTURE;
+        }
+        if(color == 2){
+            return ENTITY_DIRT_BALL_KING_GRASS_TEXTURE;
+        }
         return ENTITY_DIRT_BALL_KING_TEXTURE;
     }
 }
--- src/main/java/chongteam/soulforging/entity/EntityDirtBallKing.java
+++ src/main/java/chongteam/soulforging/entity/EntityDirtBallKing.java
@@ -1,12 +1,24 @@
 package chongteam.soulforging.entity;
 
 import chongteam.soulforging.SoulForging;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
+
+import java.util.UUID;
 
 public class EntityDirtBallKing extends EntityMob {
     public static final String ID="dirt_ball_king";
@@ -20,8 +32,8 @@ public class EntityDirtBallKing extends EntityMob {
     @Override
     protected void initEntityAI() {
         this.tasks.addTask(0,new EntityAISwimming(this));
-        this.tasks.addTask(1,new EntityAIAttackMelee(this,0.8,false));
-        this.tasks.addTask(2,new EntityAIWanderAvoidWater(this,0.2));
+        this.tasks.addTask(1,new EntityAIAttackMelee(this,1.0,false));
+        this.tasks.addTask(2,new EntityAIWanderAvoidWater(this,0.8));
         this.tasks.addTask(3,new AIChangeGrassToDirt(this));
         this.tasks.addTask(4,new EntityAIWatchClosest(this, EntityPlayer.class,8.0F));
         this.tasks.addTask(5,new EntityAILookIdle(this));
@@ -46,5 +58,61 @@ public class EntityDirtBallKing extends EntityMob {
             BlockPos blockPos=new BlockPos(this.entity.posX,this.entity.posY-0.2,this.entity.posZ);
             return this.entity.world.getBlockState(blockPos).getBlock()==Blocks.GRASS;
         }
+    }
+
+    @Override
+    protected void applyEntityAttributes(){
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(25.0);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3.0);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.3);
+    }
+
+    private static final UUID SPEED_BOOST=UUID.fromString("77b56b3e-c30c-4ee6-8321-ab5631be6381");
+
+    @Override
+    public void setAttackTarget(EntityLivingBase entity){
+        super.setAttackTarget(entity);
+        IAttributeInstance attribute=this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED);
+        if(entity == null){
+            attribute.removeModifier(SPEED_BOOST);
+        }
+        else if(attribute.getModifier(SPEED_BOOST) != null){
+            attribute.applyModifier(new AttributeModifier(SPEED_BOOST,"Speed boost",0.5,2).setSaved(false));
+        }
+    }
+
+    @Override
+    public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty,IEntityLivingData data){
+        IAttributeInstance attribute=this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH);
+        attribute.applyModifier(new AttributeModifier("Health boost",this.rand.nextInt(6),0));
+
+        this.getDataManager().set(COLOR,(byte) this.rand.nextInt(3));
+
+        return super.onInitialSpawn(difficulty,data);
+    }
+
+    private static final DataParameter<Byte> COLOR= EntityDataManager.createKey(EntityDirtBallKing.class, DataSerializers.BYTE);
+
+    @Override
+    protected void entityInit(){
+        super.entityInit();
+        this.getDataManager().register(COLOR,(byte) 0);
+    }
+
+    public byte getColor(){
+        return this.getDataManager().get(COLOR);
+    }
+
+    @Override
+    public void writeEntityToNBT(NBTTagCompound compound){
+        super.writeEntityToNBT(compound);
+        compound.setByte("Color",this.getDataManager().get(COLOR));
+    }
+
+    @Override
+    public void readEntityFromNBT(NBTTagCompound compound){
+        super.readEntityFromNBT(compound);
+        this.getDataManager().set(COLOR,compound.getByte("Color"));
     }
 }
